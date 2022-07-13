2e3b517f649fffac555b190daff065715f08a522
KANxristina <44588456+KANxristina@users.noreply.github.com>
2022-07-13 09:56:49 +0300

    Add files via upload

---
+++ PrimeNumbers/PrimeNumbers/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+       static bool PrimeNumbers(int n)
+        {
+            bool isprime = true;
+            for (int i = 2; i < 10; i++)
+            {
+                int remaining=n % i;
+                if (n!=i && remaining==0)
+                {
+                    isprime =false;
+                    return isprime;
+                     }
+            }
+            return isprime;
+        }
+        public static void AskForNumbers()
+        {
+            bool stop = false;
+            while (!stop)
+            {
+                Console.WriteLine("Give number: \n");
+                int number = Convert.ToInt32(Console.ReadLine());
+                PrimeNumbers(number);
+                if (PrimeNumbers(number))//Εαν επιστρέψει true
+                {
+                    Console.WriteLine("Prime Number\n");
+                }
+                else
+                {
+                    Console.WriteLine("NOT!!!\n");
+                }
+
+                Console.WriteLine("If you want to stop type:'STOP' \n");
+                var option = Console.ReadLine();
+                if (option == "STOP" || option == "stop" || option == "Stop")
+                {
+                    stop = true;
+                    Console.WriteLine("Bye\n");
+                    //return;
+                }
+            }         
+        }
+        static void Main(string[] args)
+        {
+            AskForNumbers();
+        }
+    }
+}
